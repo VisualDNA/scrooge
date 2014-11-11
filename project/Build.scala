28125f2497dc071e21f078645d2575da0f1406e1
Michael Donaghy <michael.donaghy@visualdna.com>
2014-11-11 17:21:46 +0000

    repository config

--- project/Build.scala
+++ project/Build.scala
@@ -9,7 +9,7 @@ import sbtassembly.Plugin._
 import AssemblyKeys._
 
 object Scrooge extends Build {
-  val libVersion = "3.17.0-VDNA-1"
+  val libVersion = "3.17.0-VDNA-2"
   val utilVersion = "6.22.0"
   val finagleVersion = "6.22.0"
 
