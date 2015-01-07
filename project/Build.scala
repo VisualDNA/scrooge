5a47f51ee93f3a87a195be6cc4c974d6db2b5b69
Michael Donaghy <michael.donaghy@visualdna.com>
2015-01-07 11:35:06 +0000

    Depend on kamon so we can use it

--- project/Build.scala
+++ project/Build.scala
@@ -206,7 +206,8 @@ object Scrooge extends Build {
     name := "scrooge-scalaz",
     libraryDependencies ++= Seq(
       "org.apache.thrift" % "libthrift" % "0.9.3-VDNA-3",
-      "org.scalaz" %% "scalaz-concurrent" % "7.1.0"
+      "org.scalaz" %% "scalaz-concurrent" % "7.1.0",
+      "io.kamon" %% "kamon-core" % "0.3.5"
     ),
     crossScalaVersions += "2.11.2"
   ).dependsOn(scroogeCore)
